38b8ebc4ec9da7e0edb2f9367fb6271b722fc610
Umut Fidan <contact.umutfidan@gmail.com>
2023-12-28 00:34:32 +0300

    webview for details

--- app/src/main/java/com/example/newsapicase/presentation/detail/NewsDetailActivity.kt
+++ app/src/main/java/com/example/newsapicase/presentation/detail/NewsDetailActivity.kt
@@ -1,10 +1,11 @@
 package com.example.newsapicase.presentation.detail
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import com.example.newsapicase.R
-import com.example.newsapicase.data.model.Article
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,18 +14,28 @@ class NewsDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_detail)
         val url = getDataFromMain()
+        setToolbar()
         openWebView(url)
     }
 
-
-    private fun getDataFromMain() : String {
-        val article = intent.extras?.get("url") as String
+    private fun getDataFromMain(): String {
+        val article = intent.extras?.getString("url") ?: ""
         return article
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     private fun openWebView(url: String) {
-        val myWebView = WebView(this@NewsDetailActivity)
-        setContentView(myWebView)
+        val myWebView = findViewById<WebView>(R.id.webview)
+        myWebView.settings.javaScriptEnabled = true
         myWebView.loadUrl(url)
+    }
+
+    private fun setToolbar() {
+        val toolbar = findViewById<View>(R.id.toolbar) as androidx.appcompat.widget.Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
