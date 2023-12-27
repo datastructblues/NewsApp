4874f08831c0391d7c331894cb00848aa8fa5ac0
Umut Fidan <contact.umutfidan@gmail.com>
2023-12-28 02:19:18 +0300

    refactor main

--- app/src/main/java/com/example/newsapicase/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/newsapicase/presentation/main/MainActivity.kt
@@ -38,11 +38,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-       // initCategoriesButtonsAndNews()
-     //   observeNewsLiveData(viewModel.categoryNews)
-     //   observeNewsLiveData(viewModel.searchNews)
         setSearchViewListener()
-       // swipeRefresh()
         onNavigationClicks()
     }
 
