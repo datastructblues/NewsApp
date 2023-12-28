61f81b888e943b3ceed7068f17587b1eda9f906b
Umut Fidan <contact.umutfidan@gmail.com>
2023-12-28 15:17:53 +0300

    minsdk level

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.newsapicase"
-        minSdk = 33
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
